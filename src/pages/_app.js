9b0e89a456275c1cfdb2c01bc91fcda10a5aae0f
manuuuskp <manojprabhuskp@gmail.com>
2023-10-02 15:05:30 +0530

    change import format

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,6 @@
-import "@/styles/globals.css";
 import { Provider } from "react-redux";
-import { store } from "./../store";
+import { store } from "@/store";
+import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
   return (
